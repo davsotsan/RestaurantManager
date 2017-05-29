60a26080df8f4274c5cd616d077fd364a8cf8e77
David Soto Sanchez <david.soto@mail.polimi.it>
2017-05-29 12:18:30 +0200

    prueba

--- app/src/main/java/com/dima/restaurantmanager/DividerItemDecoration.java
+++ app/src/main/java/com/dima/restaurantmanager/DividerItemDecoration.java
@@ -21,6 +21,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
     public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
     public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
 
+
     private Drawable mDivider;
     private int mOrientation;
 
